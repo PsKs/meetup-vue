c6af9f79f408a75cfdd0122c2776faa192abcf29
PsKs <pongsakorn.psks@gmail.com>
2018-04-27 04:42:19 +0700

    Toolbar and navigation.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <template>
     <v-app>
+        <v-toolbar dark class="primary">
+            <v-toolbar-side-icon @click="sideNav = !sideNav"></v-toolbar-side-icon>
+            <v-toolbar-title>MeetUp VUE</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-xs-only">
+                <v-btn flat>
+                    <v-icon left>supervisor_account</v-icon>
+                    View Meetup
+                </v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+        <v-navigation-drawer v-model="sideNav">
+            <v-list dense class="pt-0">
+                <v-list-tile>
+                    <v-list-tile-action>
+                        <v-icon>supervisor_account</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>View Meetup</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
     </v-app>
 </template>
 
@@ -7,6 +30,7 @@
 export default {
     data () {
         return {
+            sideNav: false
         }
     },
     name: 'App'
