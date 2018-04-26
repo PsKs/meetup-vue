2caf593a04a358e594969d302a594c02e5d51376
PsKs <pongsakorn.psks@gmail.com>
2018-04-27 05:27:03 +0700

    Dynamic menus and Route page.

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,32 @@
     <v-app>
         <v-toolbar dark class="primary">
             <v-toolbar-side-icon @click="sideNav = !sideNav"></v-toolbar-side-icon>
-            <v-toolbar-title>MeetUp VUE</v-toolbar-title>
+            <v-toolbar-title>
+                <router-link to="/" tag="span" style="cursor: pointer">MeetUp VUE</router-link>
+            </v-toolbar-title>
             <v-spacer></v-spacer>
             <v-toolbar-items class="hidden-xs-only">
-                <v-btn flat>
-                    <v-icon left>supervisor_account</v-icon>
-                    View Meetup
+                <v-btn flat v-for="item in menuItems" :key="item.title" router :to="item.link">
+                    <v-icon left>{{ item.icon }}</v-icon>
+                    {{ item.title }}
                 </v-btn>
             </v-toolbar-items>
         </v-toolbar>
-        <v-navigation-drawer v-model="sideNav">
+        <v-navigation-drawer temporary v-model="sideNav">
             <v-list dense class="pt-0">
-                <v-list-tile>
+                <v-list-tile v-for="item in menuItems" :key="item.title" router :to="item.link">
                     <v-list-tile-action>
-                        <v-icon>supervisor_account</v-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
                     </v-list-tile-action>
                     <v-list-tile-content>
-                        <v-list-tile-title>View Meetup</v-list-tile-title>
+                        <v-list-tile-title>{{ item.title }}</v-list-tile-title>
                     </v-list-tile-content>
                 </v-list-tile>
             </v-list>
         </v-navigation-drawer>
+        <main>
+            <router-view></router-view>
+        </main>
     </v-app>
 </template>
 
@@ -30,7 +35,14 @@
 export default {
     data () {
         return {
-            sideNav: false
+            sideNav: false,
+            menuItems: [
+                { icon: 'supervisor_account', title: 'View Meetups', link: '/meetups' },
+                { icon: 'room', title: 'Organize Meetups', link: '/meetup/new' },
+                { icon: 'person', title: 'Profile', link: '/profile' },
+                { icon: 'face', title: 'Sign up', link: '/signup' },
+                { icon: 'lock_open', title: 'Sign in', link: '/signin' }
+            ]
         }
     },
     name: 'App'
