e0b705a37439cd3cb501ab62e1f5cf0258cddfb5
PsKs <pongsakorn.psks@gmail.com>
2018-04-27 04:41:21 +0700

    Set Indent with 4 spaces.

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,7 @@ module.exports = {
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // Indent with 4 spaces
+    "indent": ["error", 4],
   }
 }
