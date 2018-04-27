e580103d561e32d1361b696e76659bc531a6adff
PsKs <pongsakorn.psks@gmail.com>
2018-04-27 07:13:28 +0700

    fix Navbar droped to bottom.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-    <v-app>
+    <v-app>     
+        <v-navigation-drawer temporary fixed v-model="sideNav">
+            <v-list dense class="pt-0">
+                <v-list-tile v-for="item in menuItems" :key="item.title" router :to="item.link">
+                    <v-list-tile-action>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
         <v-toolbar dark class="primary">
             <v-toolbar-side-icon @click="sideNav = !sideNav"></v-toolbar-side-icon>
             <v-toolbar-title>
@@ -13,21 +25,13 @@
                 </v-btn>
             </v-toolbar-items>
         </v-toolbar>
-        <v-navigation-drawer temporary v-model="sideNav">
-            <v-list dense class="pt-0">
-                <v-list-tile v-for="item in menuItems" :key="item.title" router :to="item.link">
-                    <v-list-tile-action>
-                        <v-icon>{{ item.icon }}</v-icon>
-                    </v-list-tile-action>
-                    <v-list-tile-content>
-                        <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
-            </v-list>
-        </v-navigation-drawer>
-        <main>
-            <router-view></router-view>
-        </main>
+        <v-content>
+            <v-container fluid>
+                <main>
+                    <router-view></router-view>
+                </main>
+            </v-container>
+        </v-content> 
     </v-app>
 </template>
 
