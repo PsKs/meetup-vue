ce18b577a8a8d28e7a65fdbb2e9c8d9363a99e3d
PsKs <pongsakorn.psks@gmail.com>
2018-05-04 05:54:14 +0700

    Uploading Images.

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -32,13 +32,21 @@
                     </v-layout>
                     <v-layout row>
                         <v-flex xs12 sm6 offset-sm3>
-                            <v-text-field 
-                                name="imageUrl" 
-                                label="Image URL" 
-                                id="image-url" 
-                                v-model="imageUrl"
-                                required>
-                            </v-text-field>
+                            <v-btn
+                                @click="onPickFile"
+                                color="blue-grey"
+                                class="white--text ml-0"
+                            >
+                            Upload Image
+                            <v-icon right dark>cloud_upload</v-icon>
+                            </v-btn>
+                            <input
+                                type="file"
+                                style="display: none"
+                                ref="fileInput"
+                                accept="image/*"
+                                @change="onFilePicked"
+                            >
                         </v-flex>
                     </v-layout>
                     <v-layout row>
@@ -75,10 +83,12 @@
                     </v-layout>
                     <v-layout>
                         <v-flex xs12 sm6 offset-sm3>
-                            <v-btn class="primary"
+                            <v-btn 
+                                class="primary ml-0"
                                 :disabled="!formIsValid"
                                 type="submit"
-                            > Create Meetup
+                            >
+                            Create Meetup
                             </v-btn>
                         </v-flex>
                     </v-layout>
@@ -99,7 +109,8 @@ export default {
             imageUrl: '',
             description: '',
             date: '',
-            time: ''
+            time: '',
+            image: null
         }
     },
     created () {
@@ -124,15 +135,34 @@ export default {
             if (!this.formIsValid) {
                 return
             }
+            if (!this.image) {
+                return
+            }
             const meetupData = {
                 title: this.title,
                 location: this.location,
-                imageUrl: this.imageUrl,
+                image: this.image,
                 description: this.description,
                 date: this.submittableDateTime
             }
             this.$store.dispatch('createMeetup', meetupData)
             this.$router.push('/meetups')
+        },
+        onPickFile () {
+            this.$refs.fileInput.click()
+        },
+        onFilePicked (event) {
+            const files = event.target.files
+            let filename = files[0].name
+            if (filename.lastIndexOf('.') <= 0) {
+                return alert('Please add a valid file!')
+            }
+            const fileReader = new FileReader()
+            fileReader.addEventListener('load', () => {
+                this.imageUrl = fileReader.result
+            })
+            fileReader.readAsDataURL(files[0])
+            this.image = files[0]
         }
     }
 }
